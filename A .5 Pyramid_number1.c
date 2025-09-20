bc05e799e41e3c2248d6ced7fe21278fd34fc51f
Amol Barde <138032586+Amolbarde@users.noreply.github.com>
2025-09-20 17:51:47 +0530

    Create A .5 Pyramid_number1.c

---
+++ A .5 Pyramid_number1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+ int i,j,k;
+ for(i=1;i<=5;i++)
+ {
+  for(j=5;j>=i;j--)
+  {
+   printf(" ");
+  }
+  for(k=1;k<=i;k++)
+  {
+   printf("%d",k);
+  }
+  for(k=i-1;k>=1;k--)
+  {
+  prinft("%d",k);
+  }
+  printf("\n");
+ }
+}
+
+
+/*
+    1
+   121
+  12321
+ 1234321
+123454321
+
