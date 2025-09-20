1740cbe9495f9cee4bdd8fa7ae81024d90ac5518
Amol Barde <138032586+Amolbarde@users.noreply.github.com>
2025-09-20 17:49:55 +0530

    Update A .4 Pyramid_number.c

--- A .4 Pyramid_number.c
+++ A .4 Pyramid_number.c
@@ -8,6 +8,29 @@ int main()
   {
    printf(" ");
   }
+  for(k=1;k<=i;k++)
+  {
+   printf("%d",k);
+  }
+  for(k=i-1;k>=1;k--)
+  {
+  prinft("%d",k);
+  }
+  printf("\n");
+ }
+}
+
+
+#include<stdio.h>
+int main()
+{
+ int i,j,k;
+ for(i=1;i<=5;i++)
+ {
+  for(j=5;j>=i;j--)
+  {
+   printf(" ");
+  }
   for(k=1;k<=i*2-1;k++)
   {
    printf("%d",k);
@@ -16,11 +39,10 @@ int main()
  }
 }
 
-
-
-//    1
-//   121
-//  12321
-// 1234321
-//123454321
+/*
+     1
+    123
+   12345
+  1234567
+ 123456789
 
