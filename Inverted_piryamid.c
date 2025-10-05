b2083e36ceec3520303da2113d8c9dd1243283f4
Amol Barde <138032586+Amolbarde@users.noreply.github.com>
2025-10-05 20:22:56 +0530

    Create Inverted_piryamid.c

---
+++ Inverted_piryamid.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+  int i,j,k;
+  for(i=0;i<=6;i++)
+  {
+   for(j=1;j<=i;j++)
+   {
+    printf(" ");
+   }
+   for(k=6;k>i*2-1;k--)
+   {
+    printf("*");
+   }
+   printf("\n");
+  }
+}
+
+// amol@amol-HP-ProBook-440-G3:~$ gcc inverted_pyramid.c 
+// amol@amol-HP-ProBook-440-G3:~$ ./a.out
+// *******
+//  *****
+//   ***
+//    *
